eb4cbb616a03e57b50b66d31ac0feebc4dc66044
yb <skyim0907@naver.com>
2024-09-23 17:22:47 +0900

    0923

--- src/main/java/hello/core/OrderApp.java
+++ src/main/java/hello/core/OrderApp.java
@@ -7,12 +7,19 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.order.Order;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class OrderApp {
 
     public static void main(String[] args) {
-        MemberService memberService = new MemberServiceImpl();
-        OrderService orderService = new OrderServiceImpl();
+        //AppConfig appConfig = new AppConfig();
+        //MemberService memberService = appConfig.memberService();
+        //OrderService orderService = appConfig.orderService();
+
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = (MemberService) applicationContext.getBean("memberService");
+        OrderService orderService = (OrderService) applicationContext.getBean("orderService");
 
         Long memberId = 1L;
         Member member = new Member(memberId, "member A", Grade.VIP);
